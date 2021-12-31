b343fdb62bcff9f761783ec0d456a31ed93565e6
Pi Delport <pjdelport@gmail.com>
2021-12-31 14:19:53 +0200

    feat(haskell/easy): bob iteration 2, with Data.Text and category type

--- haskell/easy/bob/src/Bob.hs
+++ haskell/easy/bob/src/Bob.hs
@@ -1,23 +1,33 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Bob (responseFor) where
 
-import Data.Char (isAlpha, isSpace, isUpper)
-import Data.List (dropWhileEnd)
+import Data.Char (isAlpha, isUpper)
+import Data.Text (Text)
+import qualified Data.Text as T
+
+-- | Bob's conversational categories.
+data Category = Question | Yelled | YelledQuestion | Silence | Other
 
 -- | Bob's response for the given input.
-responseFor :: String -> String
-responseFor xs
-  | question && yelling = "Calm down, I know what I'm doing!"
-  | question = "Sure."
-  | yelling = "Whoa, chill out!"
-  | nothing = "Fine. Be that way!"
-  | otherwise = "Whatever."
-  where
-    text = stripSpace xs
-    nothing = null text
-    question = not nothing && last text == '?'
-    yelling = not (null letters) && all isUpper letters
-    letters = filter isAlpha text
+responseFor :: Text -> Text
+responseFor xs = case categorise xs of
+  Question -> "Sure."
+  Yelled -> "Whoa, chill out!"
+  YelledQuestion -> "Calm down, I know what I'm doing!"
+  Silence -> "Fine. Be that way!"
+  Other -> "Whatever."
 
--- | Strip leading and trailing whitespace from a string.
-stripSpace :: String -> String
-stripSpace = dropWhileEnd isSpace . dropWhile isSpace
+-- | Categorise the given input, according to Bob.
+categorise :: Text -> Category
+categorise input
+  | question && yelling = YelledQuestion
+  | question = Question
+  | yelling = Yelled
+  | T.null stripped = Silence
+  | otherwise = Other
+  where
+    stripped = T.strip input
+    question = "?" `T.isSuffixOf` stripped
+    yelling = not (T.null letters) && T.all isUpper letters
+    letters = T.filter isAlpha stripped
