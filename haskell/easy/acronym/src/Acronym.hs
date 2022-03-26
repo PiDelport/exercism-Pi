9a0586c7ae269329f48695ce041aba419fe6cb17
Pi Delport <pjdelport@gmail.com>
2022-03-26 23:55:05 +0200

    feat(haskell): easy/acronym, iteration 2 (word-based algorithm)

--- haskell/easy/acronym/src/Acronym.hs
+++ haskell/easy/acronym/src/Acronym.hs
@@ -1,14 +1,45 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module Acronym (abbreviate) where
+module Acronym (abbreviate, abbreviateByWords, abbreviateByBoundaries) where
 
-import Data.Char (isLetter, isLower, isUpper)
+import Data.Char (isLetter, isLower, isUpper, toUpper)
 import Data.Text (Text)
 import qualified Data.Text as T
 
 -- | Convert a phrase to its acronym.
 abbreviate :: Text -> Text
-abbreviate = T.toUpper . filterWithPrev isWordStart ' '
+abbreviate = abbreviateByWords
+
+-- * Word-based algorithm
+
+-- | Convert a phrase to its acronym. (Word-based algorithm)
+abbreviateByWords :: Text -> Text
+abbreviateByWords = T.concat . map abbreviateWord . wordsByLetter
+
+-- | Like 'T.words', but include based on 'isLetter' (and @'\''@).
+-- >>> wordsByLetter "Halley's Comet"
+-- ["Halley's","Comet"]
+wordsByLetter :: Text -> [Text]
+wordsByLetter = T.words . T.map replace
+  where
+    replace c | isLetter c = c
+    replace '\'' = '\''
+    replace _ = ' '
+
+-- | Abbreviate a single word.
+-- >>> abbreviateWord <$> ["lowercase","UPPERCASE","camelCase"]
+-- ["L","U","CC"]
+abbreviateWord :: Text -> Text
+abbreviateWord t
+  | T.all isUpper t = T.take 1 t
+  | Just (c, t') <- T.uncons t = toUpper c `T.cons` T.filter isUpper t'
+  | otherwise = t
+
+-- * Boundary-based algorithm
+
+-- | Convert a phrase to its acronym. (Boundary-based algorithm)
+abbreviateByBoundaries :: Text -> Text
+abbreviateByBoundaries = T.toUpper . filterWithPrev isWordStart ' '
 
 -- | True if a character boundary starts a word.
 isWordStart :: Char -> Char -> Bool
